7f23b402bd14e3c96e1e38f1e5540195e62b30b7
hruthikgurram <79998454+hruthikgurram@users.noreply.github.com>
2022-07-10 19:56:49 +0530

    Add files via upload

---
+++ build/precache-manifest.4c617b8df0b3159cbb72f73fd8f1231d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4b8f78446e0909c48ffd24643b6da21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28b6bc39e6111cfe3cac",
+    "url": "/static/css/main.3944e690.chunk.css"
+  },
+  {
+    "revision": "d57684a2455f1b6710f1",
+    "url": "/static/js/2.65cfc8ef.chunk.js"
+  },
+  {
+    "revision": "28b6bc39e6111cfe3cac",
+    "url": "/static/js/main.0339304e.chunk.js"
+  },
+  {
+    "revision": "d6c8b0338a16e1e82302",
+    "url": "/static/js/runtime-main.60d0dddc.js"
+  }
+]);
